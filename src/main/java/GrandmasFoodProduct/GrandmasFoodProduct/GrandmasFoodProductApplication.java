afa62c8b96e46b524b66bdbf4ff345c71640bf93
Sebastian Sepulveda Patino <s.sepulveda01@globant.com>
2025-02-13 15:59:50 -0500

    MicroserviceProduct creation

---
+++ src/main/java/GrandmasFoodProduct/GrandmasFoodProduct/GrandmasFoodProductApplication.java
@@ -0,0 +1,13 @@
+package GrandmasFoodProduct.GrandmasFoodProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandmasFoodProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandmasFoodProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GrandmasFoodProduct/GrandmasFoodProduct/GrandmasFoodProductApplicationTests.java
@@ -0,0 +1,13 @@
+package GrandmasFoodProduct.GrandmasFoodProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandmasFoodProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
