20886da06c11a15d28d32a24ded53945e80f52a8
s.sepulveda01 <s.sepulveda01@globant.com>
2025-02-19 11:37:55 -0500

    fix($maven): change from gradle to maven

--- src/main/java/GrandmasFoodProduct/GrandmasFoodProduct/application/mapper/ProductRequestMapper.java
+++ src/main/java/GrandmasFoodProduct/GrandmasFoodProduct/application/mapper/ProductRequestMapper.java
@@ -3,7 +3,8 @@ package GrandmasFoodProduct.GrandmasFoodProduct.application.mapper;
 import GrandmasFoodProduct.GrandmasFoodProduct.application.dto.ProductRequest;
 import GrandmasFoodProduct.GrandmasFoodProduct.domain.models.Product;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
+
 
 @Mapper(componentModel = "spring")
 public interface ProductRequestMapper {
--- src/main/java/GrandmasFoodProduct/GrandmasFoodProduct/infrastructure/jpa/mapper/ProductEntityMapper.java
+++ src/main/java/GrandmasFoodProduct/GrandmasFoodProduct/infrastructure/jpa/mapper/ProductEntityMapper.java
@@ -3,12 +3,14 @@ package GrandmasFoodProduct.GrandmasFoodProduct.infrastructure.jpa.mapper;
 import GrandmasFoodProduct.GrandmasFoodProduct.domain.models.Product;
 import GrandmasFoodProduct.GrandmasFoodProduct.infrastructure.jpa.entity.ProductEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductEntityMapper {
+    ProductEntityMapper INSTANCE = Mappers.getMapper(ProductEntityMapper.class);
 
 
     ProductEntity toEntity(Product product);
